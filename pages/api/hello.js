092e7ddad1980ef4002fa2f30bc474753b0199da
cartermp <pcarter@fastmail.com>
2021-10-01 19:39:56 -0700

    update

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
+  throw Error('whoopsie')
   res.status(200).json({ name: 'poopy' })
 }
