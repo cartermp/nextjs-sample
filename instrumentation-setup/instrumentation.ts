5385b8c790ad489efb00dad55f1e4aa7f488f4f4
cartermp <pcarter@fastmail.com>
2021-10-06 21:47:23 -0700

    yeet2

--- instrumentation-setup/instrumentation.ts
+++ instrumentation-setup/instrumentation.ts
@@ -9,7 +9,7 @@ const metadata = new Metadata()
 metadata.set('x-honeycomb-team', process.env.HNY_KEY);
 metadata.set('x-honeycomb-dataset', process.env.VERCEL_SAMPLE_DATASET);
 const traceExporter = new CollectorTraceExporter({
-  url: 'grpc://api.honeycomb.io:443/',
+  url: 'grpc://https://api-dogfood.honeycomb.io/',
   credentials: credentials.createSsl(),
   metadata
 });
