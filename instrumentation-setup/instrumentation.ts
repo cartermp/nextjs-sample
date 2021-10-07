4c8d9087dc9564cd1312b66ec0745cee4691b4d4
cartermp <pcarter@fastmail.com>
2021-10-06 21:38:08 -0700

    let's see if this works

---
+++ instrumentation-setup/instrumentation.ts
@@ -0,0 +1,21 @@
+import process from 'process'
+import opentelemetry from '@opentelemetry/api'
+import { Metadata, credentials } from '@grpc/grpc-js'
+import { CollectorTraceExporter } from '@opentelemetry/exporter-collector-grpc'
+import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node'
+import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base'
+
+const metadata = new Metadata()
+metadata.set('x-honeycomb-team', process.env.HNY_KEY);
+metadata.set('x-honeycomb-dataset', process.env.VERCEL_SAMPLE_DATASET);
+const traceExporter = new CollectorTraceExporter({
+  url: 'grpc://api.honeycomb.io:443/',
+  credentials: credentials.createSsl(),
+  metadata
+});
+
+const provider = new NodeTracerProvider();
+provider.addSpanProcessor(new SimpleSpanProcessor(traceExporter))
+provider.register();
+
+export const requestTracer = opentelemetry.trace.getTracer('request-tracer')
