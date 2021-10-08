ddb0074e0cb3d25a153070f4449259cc8e5ab69f
cartermp <pcarter@fastmail.com>
2021-10-08 12:07:24 -0700

    Updates -- maybe better spans?

--- pages/api/people/[id].ts
+++ pages/api/people/[id].ts
@@ -1,49 +1,48 @@
-import { SpanStatusCode } from '@opentelemetry/api'
+import { SpanStatusCode, Span } from '@opentelemetry/api'
 import { VercelRequest, VercelResponse } from '@vercel/node'
 import { people } from './../data'
 import { requestTracer } from '../../../instrumentation-setup/functions-instrumentation'
 
-const handleRequest = (request: VercelRequest, response: VercelResponse) => {
+const handleRequest = (request: VercelRequest, response: VercelResponse, span: Span) => {
     const id = request.query["id"]
+
+    span.setAttribute('people-id', id)
+
     if (typeof id !== "string") {
-        response.status(404).send(`Bad request: ${request.query}`)
-        return
+      span.setStatus({ code: SpanStatusCode.ERROR })
+      response.status(404).send(`Bad request: ${request.query}`)
+      return
     }
 
     const person = people.find(p => p.id === id)
     if (!person) {
-        response.status(404).send(`Bad ID: ${id}`)
-        return
+      span.setStatus({ code: SpanStatusCode.ERROR })
+      response.status(404).send(`Bad ID: ${id}`)
+      return
     }
 
     response.status(200).send(person)
 }
 
 const personHandler = (request: VercelRequest, response: VercelResponse) => {
-    requestTracer.startActiveSpan('request', span => {
-        span.setStatus({
-          code: SpanStatusCode.OK,
-        })
-    
-        span.setAttributes({
-          method: request.method,
-          path: request.url,
-          query: JSON.stringify(request.query),
-          body: JSON.stringify(request.body),
-          headers: JSON.stringify(request.headers),
-        })
-    
-        try {
-            handleRequest(request, response)
-        } catch (error) {
-          span.setStatus({
-            code: SpanStatusCode.ERROR,
-          })
-          request.statusCode = 500
-        } finally {
-          span.end()
-        }
-      })
-};
+  const span = requestTracer.startSpan('person-request')
+  
+  span.setAttributes({
+    method: request.method,
+    path: request.url,
+    query: JSON.stringify(request.query),
+    body: JSON.stringify(request.body),
+    headers: JSON.stringify(request.headers)
+  })
+
+  try {
+    handleRequest(request, response, span)
+  } catch (error) {
+    span.setStatus({ code: SpanStatusCode.ERROR })
+    response.statusCode = 500
+  } finally {
+    span.end()
+  }
+}
 
 export default personHandler
--- pages/api/people/index.ts
+++ pages/api/people/index.ts
@@ -3,31 +3,25 @@ import { VercelRequest, VercelResponse } from '@vercel/node'
 import { people } from './../data'
 import { requestTracer } from '../../../instrumentation-setup/functions-instrumentation'
 
-const peopleHanlder = (request: VercelRequest, response: VercelResponse) => {
-    requestTracer.startActiveSpan('request', span => {
-        span.setStatus({
-          code: SpanStatusCode.OK,
-        })
-    
-        span.setAttributes({
-          method: request.method,
-          path: request.url,
-          query: JSON.stringify(request.query),
-          body: JSON.stringify(request.body),
-          headers: JSON.stringify(request.headers),
-        })
-    
-        try {
-            response.status(200).send(people);
-        } catch (error) {
-          span.setStatus({
-            code: SpanStatusCode.ERROR,
-          })
-          request.statusCode = 500
-        } finally {
-          span.end()
-        }
-      })
-};
+const peopleHandler = (request: VercelRequest, response: VercelResponse) => {
+  const span = requestTracer.startSpan('people-request')
+  
+  span.setAttributes({
+    method: request.method,
+    path: request.url,
+    query: JSON.stringify(request.query),
+    body: JSON.stringify(request.body),
+    headers: JSON.stringify(request.headers)
+  })
 
-export default peopleHanlder
+  try {
+    response.status(200).send(people)
+  } catch (error) {
+    span.setStatus({ code: SpanStatusCode.ERROR })
+    response.statusCode = 500
+  } finally {
+    span.end()
+  }
+}
+
+export default peopleHandler
